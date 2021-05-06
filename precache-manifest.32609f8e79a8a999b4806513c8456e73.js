8f84c13746923dab7ae57727d386735cdd729dac
Andrey <s.adnrey-klg@yandex.ru>
2021-05-06 23:26:33 +0300

    Updates

---
+++ precache-manifest.32609f8e79a8a999b4806513c8456e73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41898ffddcbdb5756d48c3dc94d1993a",
+    "url": "/DonSaliere/ES_Test_Front/index.html"
+  },
+  {
+    "revision": "7818de47ebd8fb1aaa4c",
+    "url": "/DonSaliere/ES_Test_Front/static/css/3.d2754321.chunk.css"
+  },
+  {
+    "revision": "a65e7c4bfcc2078b3d73",
+    "url": "/DonSaliere/ES_Test_Front/static/css/main.b5b335a7.chunk.css"
+  },
+  {
+    "revision": "a7b92750b09418902fda",
+    "url": "/DonSaliere/ES_Test_Front/static/js/2.930ba658.chunk.js"
+  },
+  {
+    "revision": "7818de47ebd8fb1aaa4c",
+    "url": "/DonSaliere/ES_Test_Front/static/js/3.afd2ce37.chunk.js"
+  },
+  {
+    "revision": "9abd151946b78e6412b3",
+    "url": "/DonSaliere/ES_Test_Front/static/js/4.656cec96.chunk.js"
+  },
+  {
+    "revision": "45dbff12f0e8cc810e23",
+    "url": "/DonSaliere/ES_Test_Front/static/js/5.f45b3b4e.chunk.js"
+  },
+  {
+    "revision": "a65e7c4bfcc2078b3d73",
+    "url": "/DonSaliere/ES_Test_Front/static/js/main.9c99cbea.chunk.js"
+  },
+  {
+    "revision": "f882215e0da07cb6418a",
+    "url": "/DonSaliere/ES_Test_Front/static/js/runtime~main.52d88f50.js"
+  }
+]);
